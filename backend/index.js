daa5df8c390a79c12fc0043dd842cd0ceddc345a
Arnav Borborah <arnavborborah11@gmail.com>
2021-05-15 14:58:24 -0400

    Set up linting/formatting

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,13 @@
-const express = require("express")
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello, World!")
-})
+  res.send('Hello, World!');
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-})
-
+  console.log(`Server started on port ${PORT}`);
+});
