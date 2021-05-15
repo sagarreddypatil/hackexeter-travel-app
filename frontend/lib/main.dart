8efdaadc2e0b82e0259bb3247eb79422a5db43b5
shricharanks108 <62522322+shricharanks108@users.noreply.github.com>
2021-05-15 17:33:08 -0400

    add BottomNav

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -34,6 +34,73 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Map',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Places',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Meme',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Settings',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    if (_selectedIndex == 1) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => PlacesPage()),
+      );
+    }
+
+    // if (_selectedIndex == 0) {
+    //   Navigator.push(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => PlacesPage()),
+    //   );
+    // }
+    //
+    // if (_selectedIndex == 2) {
+    //   Navigator.push(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => PlacesPage()),
+    //   );
+    // }
+    //
+    // if (_selectedIndex == 3) {
+    //   Navigator.push(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => PlacesPage()),
+    //   );
+    // }
+    //
+    // if (_selectedIndex == 4) {
+    //   Navigator.push(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => PlacesPage()),
+    //   );
+    // }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,13 +108,37 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title!),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text('Places'),
-          onPressed: () {
-            goToPlacesPage();
-          },
-        ),
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map ),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.place),
+            label: 'Places',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Meme',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined ),
+            label: 'User Stats',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
       ),
     );
   }
+
 }
